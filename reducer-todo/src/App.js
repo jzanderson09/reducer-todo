df7a30ee84fe6d5fcd16178145432f59d3fb3072
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-01-29 22:19:24 -0700

    Step 2:  Set up state in you component.  Able to set up redux store, initialState, passed through reducer and set up state, mapped to props for App and rendered via map.

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <h1>App!</h1>
+      {props.tasks.map(task => {
+        return (
+          <div>
+            <h3>{task.item}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    tasks: state.tasks
+  }
+}
+
+export default connect(mapStateToProps, {})(App);
--- reducer-todo/src/reducers/index.js
+++ reducer-todo/src/reducers/index.js
@@ -1,4 +1,4 @@
-export function appReducer(state = initialState, action) {
+export default function appReducer(state = initialState, action) {
     switch (action.type) {
         default:
             return state;
@@ -6,20 +6,22 @@ export function appReducer(state = initialState, action) {
 }
 
 //initial State Object:
-export const initialStateObj = [
-    {
-        item: 'Learn about reducers',
-        completed: false,
-        id: Date.now()
-    },
-    {
-        item: 'Do Some Laundry',
-        completed: false,
-        id: Date.now()*10
-    },
-    {
-        item: 'Learn Redux',
-        completed: false,
-        id:  Date.now()*23
-    }
-];
+export const initialState = {
+    tasks: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: Date.now()
+        },
+        {
+            item: 'Do Some Laundry',
+            completed: false,
+            id: Date.now()*10
+        },
+        {
+            item: 'Learn Redux',
+            completed: false,
+            id:  Date.now()*23
+        }
+    ]
+};
